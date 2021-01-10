7781524e78f490fe0338f6ea774101b3dbcc3a35
Adam Romański <adam.romanski@netguru.com>
2021-01-10 20:56:31 +0100

    01.1-final

--- 01/index.js
+++ 01/index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const Paragraph = () => {
+  return React.createElement(
+    'p',
+    {},
+    'Lorem ipsum dolor sit amet',
+  );
+};
+
+const appContainer = document.querySelector('#app');
+
+ReactDOM.render(React.createElement(Paragraph), appContainer);
