89bea7368c306d966d0c5af75510842248c08be3
Dawid Pietruszewski <dpietruszewski12@gmail.com>
2023-03-27 19:11:42 +0200

    Solution to first task

--- 01/index.js
+++ 01/index.js
@@ -1,36 +1,44 @@
-'use strict';
+"use strict";
 
 const data = [
   {
     name: "Adam",
     age: 28,
-    group: "Sem I / Group B"
+    group: "Sem I / Group B",
   },
   {
     name: "Roman",
     age: 30,
-    group: "Sem III / Group C"
+    group: "Sem III / Group C",
   },
   {
     name: "Ania",
     age: 27,
-    group: "Sem I / Group A"
+    group: "Sem I / Group A",
   },
   {
     name: "Karol",
     age: 31,
-    group: "Sem IV / Group A"
+    group: "Sem IV / Group A",
   },
 ];
 
 const Users = () => {
   return (
     <div>
-      <h1>Map users from data array</h1>
+      {data.map((item) => {
+        return (
+          <div key={item.name}>
+            <h1>{item.name}</h1>
+            <p>{item.age}</p>
+            <p>{item.group}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
 
-const appContainer = document.querySelector('#app');
+const appContainer = document.querySelector("#app");
 
 ReactDOM.render(<Users />, appContainer);
